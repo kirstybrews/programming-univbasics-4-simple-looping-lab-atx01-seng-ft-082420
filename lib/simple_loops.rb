82944b8a71eaba2ccfc6c80f568d979d4e8a4cd0
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-09 22:46:41 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,3 +17,12 @@ def output_array(array)
     counter += 1 
   end
 end
+
+def return_string_array(array)
+  counter = 0 
+  while array[counter] do
+    array[counter].to_s 
+    counter += 1 
+  end
+  puts array
+end
